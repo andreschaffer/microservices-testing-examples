c1ed5ddcba4ffdea09dc7a41db0549449e20494b
Andre Schaffer <andre.schaffer@gmail.com>
2017-11-13 14:02:34 +0100

    Create an integration test base for wmes

---
+++ welcome-member-email-service/src/test/java/welcomememberemailservice/it/IntegrationTestBase.java
@@ -0,0 +1,55 @@
+package welcomememberemailservice.it;
+
+import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
+
+import com.github.charithe.kafka.EphemeralKafkaBroker;
+import com.github.charithe.kafka.KafkaJunitRule;
+import io.dropwizard.testing.junit.DropwizardAppRule;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.rules.RuleChain;
+import org.subethamail.wiser.Wiser;
+import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceApplication;
+import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceConfiguration;
+
+public abstract class IntegrationTestBase {
+
+  protected static final int SMTP_SERVER_PORT = 2525;
+  protected static final String KAFKA_HOST = "localhost";
+  protected static final int KAFKA_PORT = 9092;
+  protected static final String SPECIAL_MEMBERSHIP_TOPIC = "special-membership-topic";
+  protected static final String WELCOME_EMAIL_GROUP_ID = "welcome-member-email-consumer";
+
+  protected static final EphemeralKafkaBroker KAFKA_BROKER = EphemeralKafkaBroker.create(KAFKA_PORT);
+  protected static final KafkaJunitRule KAFKA_RULE = new KafkaJunitRule(KAFKA_BROKER);
+
+  protected static final DropwizardAppRule<WelcomeMemberEmailServiceConfiguration> SERVICE_RULE =
+      new DropwizardAppRule<>(WelcomeMemberEmailServiceApplication.class, resourceFilePath("integration.yml"));
+
+  @ClassRule
+  public static final RuleChain RULES = RuleChain.outerRule(KAFKA_RULE).around(SERVICE_RULE);
+
+  protected Wiser smtpServer;
+
+  @Before
+  public void setUp() throws Exception {
+    smtpServer = new Wiser(SMTP_SERVER_PORT);
+    smtpServer.start();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    smtpServer.stop();
+  }
+
+  protected void publishMessageAndWaitToBeConsumed(String topic, String message, String groupId) {
+    KafkaOffsets kafkaOffsets = new KafkaOffsets(KAFKA_HOST, KAFKA_RULE.helper().kafkaPort());
+    long previousOffset = Math.max(kafkaOffsets.readOffset(topic, groupId), 0);
+    KAFKA_RULE.helper().produceStrings(topic, message);
+    await().atMost(30, SECONDS).until(() -> kafkaOffsets.readOffset(topic, groupId), equalTo(previousOffset + 1));
+  }
+}
--- welcome-member-email-service/src/test/java/welcomememberemailservice/it/pacts/MemberSignedUpEventPactIT.java
+++ welcome-member-email-service/src/test/java/welcomememberemailservice/it/pacts/MemberSignedUpEventPactIT.java
@@ -1,17 +1,8 @@
 package welcomememberemailservice.it.pacts;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
-import static welcomememberemailservice.it.IntegrationEnvironment.INTEGRATION_YML;
-import static welcomememberemailservice.it.IntegrationEnvironment.KAFKA_HOST;
-import static welcomememberemailservice.it.IntegrationEnvironment.KAFKA_PORT;
-import static welcomememberemailservice.it.IntegrationEnvironment.SMTP_SERVER_PORT;
-import static welcomememberemailservice.it.IntegrationEnvironment.SPECIAL_MEMBERSHIP_TOPIC;
-import static welcomememberemailservice.it.IntegrationEnvironment.WELCOME_EMAIL_GROUP_ID;
 import static welcomememberemailservice.it.pacts.PactConstants.SPECIAL_MEMBERSHIP_SERVICE;
 import static welcomememberemailservice.it.pacts.PactConstants.WELCOME_MEMBER_EMAIL_SERVICE;
 
@@ -21,49 +12,17 @@ import au.com.dius.pact.consumer.Pact;
 import au.com.dius.pact.consumer.PactVerification;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.model.v3.messaging.MessagePact;
-import com.github.charithe.kafka.EphemeralKafkaBroker;
-import com.github.charithe.kafka.KafkaJunitRule;
-import io.dropwizard.testing.junit.DropwizardAppRule;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.RuleChain;
-import org.subethamail.wiser.Wiser;
-import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceApplication;
-import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceConfiguration;
-import welcomememberemailservice.it.KafkaOffsets;
+import welcomememberemailservice.it.IntegrationTestBase;
 
-public class MemberSignedUpEventPactIT {
-
-    private static final EphemeralKafkaBroker KAFKA_BROKER = EphemeralKafkaBroker.create(KAFKA_PORT);
-    private static final KafkaJunitRule KAFKA_RULE = new KafkaJunitRule(KAFKA_BROKER);
-
-    private static final DropwizardAppRule<WelcomeMemberEmailServiceConfiguration> SERVICE_RULE =
-            new DropwizardAppRule<>(WelcomeMemberEmailServiceApplication.class, INTEGRATION_YML);
-
-    @ClassRule
-    public static final RuleChain RULES = RuleChain.outerRule(KAFKA_RULE).around(SERVICE_RULE);
+public class MemberSignedUpEventPactIT extends IntegrationTestBase {
 
     @Rule
     public final MessagePactProviderRule specialMembershipServiceRule =
             new MessagePactProviderRule(SPECIAL_MEMBERSHIP_SERVICE, this);
-
-    private Wiser smtpServer;
-
-    @Before
-    public void setUp() throws Exception {
-        smtpServer = new Wiser(SMTP_SERVER_PORT);
-        smtpServer.start();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        smtpServer.stop();
-    }
 
     @Pact(consumer = WELCOME_MEMBER_EMAIL_SERVICE, provider = SPECIAL_MEMBERSHIP_SERVICE)
     public MessagePact newMemberTonyStark(MessagePactBuilder builder) {
@@ -86,12 +45,5 @@ public class MemberSignedUpEventPactIT {
         String memberSignedUpEvent = new String(specialMembershipServiceRule.getMessage(), UTF_8);
         publishMessageAndWaitToBeConsumed(SPECIAL_MEMBERSHIP_TOPIC, memberSignedUpEvent, WELCOME_EMAIL_GROUP_ID);
         assertThat(smtpServer.getMessages(), hasSize(1));
-    }
-
-    private void publishMessageAndWaitToBeConsumed(String topic, String message, String groupId) {
-        KafkaOffsets kafkaOffsets = new KafkaOffsets(KAFKA_HOST, KAFKA_RULE.helper().kafkaPort());
-        long previousOffset = Math.max(kafkaOffsets.readOffset(topic, groupId), 0);
-        KAFKA_RULE.helper().produceStrings(topic, message);
-        await().atMost(30, SECONDS).until(() -> kafkaOffsets.readOffset(topic, groupId), equalTo(previousOffset + 1));
     }
 }
