05724be719f8323baa772d0d3ca03741016e1d39
Andre Schaffer <andre.schaffer@gmail.com>
2017-11-13 16:59:40 +0100

    Fix indentation in wmes

--- welcome-member-email-service/src/test/java/welcomememberemailservice/it/IntegrationTestBase.java
+++ welcome-member-email-service/src/test/java/welcomememberemailservice/it/IntegrationTestBase.java
@@ -23,58 +23,58 @@ import welcomememberemailservice.it.smtp.SmtpServerRule;
 
 public abstract class IntegrationTestBase {
 
-  private static final String INTEGRATION_YML = resourceFilePath("integration.yml");
-  private static final String KAFKA_HOST = "localhost";
-  private static final int KAFKA_PORT = 9092;
-  private static final String SPECIAL_MEMBERSHIP_TOPIC = "special-membership-topic";
-  private static final String WELCOME_EMAIL_GROUP_ID = "welcome-member-email-consumer";
-  private static final int SMTP_SERVER_PORT = 2525;
+    private static final String INTEGRATION_YML = resourceFilePath("integration.yml");
+    private static final String KAFKA_HOST = "localhost";
+    private static final int KAFKA_PORT = 9092;
+    private static final String SPECIAL_MEMBERSHIP_TOPIC = "special-membership-topic";
+    private static final String WELCOME_EMAIL_GROUP_ID = "welcome-member-email-consumer";
+    private static final int SMTP_SERVER_PORT = 2525;
 
-  private static final EphemeralKafkaBroker KAFKA_BROKER = EphemeralKafkaBroker.create(KAFKA_PORT);
-  private static final KafkaJunitRule KAFKA_RULE = new KafkaJunitRule(KAFKA_BROKER);
-  private static final SmtpServerRule SMTP_SERVER_RULE = new SmtpServerRule(SMTP_SERVER_PORT);
-  private static final DropwizardAppRule<WelcomeMemberEmailServiceConfiguration> SERVICE_RULE =
-      new DropwizardAppRule<>(WelcomeMemberEmailServiceApplication.class, INTEGRATION_YML);
+    private static final EphemeralKafkaBroker KAFKA_BROKER = EphemeralKafkaBroker.create(KAFKA_PORT);
+    private static final KafkaJunitRule KAFKA_RULE = new KafkaJunitRule(KAFKA_BROKER);
+    private static final SmtpServerRule SMTP_SERVER_RULE = new SmtpServerRule(SMTP_SERVER_PORT);
+    private static final DropwizardAppRule<WelcomeMemberEmailServiceConfiguration> SERVICE_RULE =
+        new DropwizardAppRule<>(WelcomeMemberEmailServiceApplication.class, INTEGRATION_YML);
 
-  @ClassRule
-  public static final RuleChain RULES = RuleChain
-      .outerRule(KAFKA_RULE)
-      .around(SMTP_SERVER_RULE)
-      .around(SERVICE_RULE);
+    @ClassRule
+    public static final RuleChain RULES = RuleChain
+        .outerRule(KAFKA_RULE)
+        .around(SMTP_SERVER_RULE)
+        .around(SERVICE_RULE);
 
-  private List<WiserMessage> emailsBeforeTest;
+    private List<WiserMessage> emailsBeforeTest;
 
-  @Before
-  public void setUp() throws Exception {
-      emailsBeforeTest = getEmails();
-  }
+    @Before
+    public void setUp() throws Exception {
+        emailsBeforeTest = getEmails();
+    }
 
-  private List<WiserMessage> getEmails() {
-      return new ArrayList<>(SMTP_SERVER_RULE.getSmtpServer().getMessages());
-  }
+    private List<WiserMessage> getEmails() {
+        return new ArrayList<>(SMTP_SERVER_RULE.getSmtpServer().getMessages());
+    }
 
 
-  protected void assertNoEmailWasSent() {
-      assertThat(getEmails(), hasSize(emailsBeforeTest.size()));
-  }
+    protected void assertNoEmailWasSent() {
+        assertThat(getEmails(), hasSize(emailsBeforeTest.size()));
+    }
 
-  protected void assertAnEmailWasSent() {
-      assertThat(getEmails(), hasSize(emailsBeforeTest.size() + 1));
-  }
+    protected void assertAnEmailWasSent() {
+        assertThat(getEmails(), hasSize(emailsBeforeTest.size() + 1));
+    }
 
-  protected WiserMessage getLastSentEmail() {
-      return getEmails().get(getEmails().size() - 1);
-  }
+    protected WiserMessage getLastSentEmail() {
+        return getEmails().get(getEmails().size() - 1);
+    }
 
-  protected void publishMembershipMessageAndWaitToBeConsumed(String message) {
-      publishMessageAndWaitToBeConsumed(SPECIAL_MEMBERSHIP_TOPIC, message, WELCOME_EMAIL_GROUP_ID);
-  }
+    protected void publishMembershipMessageAndWaitToBeConsumed(String message) {
+        publishMessageAndWaitToBeConsumed(SPECIAL_MEMBERSHIP_TOPIC, message, WELCOME_EMAIL_GROUP_ID);
+    }
 
-  protected void publishMessageAndWaitToBeConsumed(String topic, String message, String groupId) {
-      KafkaOffsets kafkaOffsets = new KafkaOffsets(KAFKA_HOST, KAFKA_RULE.helper().kafkaPort());
-      long previousOffset = Math.max(kafkaOffsets.readOffset(topic, groupId), 0);
-      KAFKA_RULE.helper().produceStrings(topic, message);
-      await().atMost(10, SECONDS).until(
-          () -> kafkaOffsets.readOffset(topic, groupId), equalTo(previousOffset + 1));
-  }
+    protected void publishMessageAndWaitToBeConsumed(String topic, String message, String groupId) {
+        KafkaOffsets kafkaOffsets = new KafkaOffsets(KAFKA_HOST, KAFKA_RULE.helper().kafkaPort());
+        long previousOffset = Math.max(kafkaOffsets.readOffset(topic, groupId), 0);
+        KAFKA_RULE.helper().produceStrings(topic, message);
+        await().atMost(10, SECONDS).until(
+            () -> kafkaOffsets.readOffset(topic, groupId), equalTo(previousOffset + 1));
+    }
 }
--- welcome-member-email-service/src/test/java/welcomememberemailservice/it/smtp/SmtpServerRule.java
+++ welcome-member-email-service/src/test/java/welcomememberemailservice/it/smtp/SmtpServerRule.java
@@ -5,27 +5,27 @@ import org.subethamail.wiser.Wiser;
 
 public class SmtpServerRule extends ExternalResource {
 
-  private final int port;
-  private Wiser smtpServer;
+    private final int port;
+    private Wiser smtpServer;
 
-  public SmtpServerRule(final int port) {
-      this.port = port;
-  }
+    public SmtpServerRule(int port) {
+        this.port = port;
+    }
 
-  @Override
-  protected void before() throws Throwable {
-      smtpServer = new Wiser(port);
-      smtpServer.start();
-  }
+    @Override
+    protected void before() throws Throwable {
+        smtpServer = new Wiser(port);
+        smtpServer.start();
+    }
 
-  @Override
-  protected void after() {
-      if (smtpServer != null) {
-        smtpServer.stop();
-      }
-  }
+    @Override
+    protected void after() {
+        if (smtpServer != null) {
+          smtpServer.stop();
+        }
+    }
 
-  public Wiser getSmtpServer() {
-      return smtpServer;
-  }
+    public Wiser getSmtpServer() {
+        return smtpServer;
+    }
 }
