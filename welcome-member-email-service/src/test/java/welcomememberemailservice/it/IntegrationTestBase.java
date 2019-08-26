ffa14d20d82583318e7ce2086028400cb7f1b443
Andre Schaffer <andre.schaffer@gmail.com>
2019-08-26 23:27:13 +0200

    Log when publishing and waiting for message to be consumed

--- welcome-member-email-service/src/test/java/welcomememberemailservice/it/IntegrationTestBase.java
+++ welcome-member-email-service/src/test/java/welcomememberemailservice/it/IntegrationTestBase.java
@@ -4,6 +4,7 @@ import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
 import static java.util.concurrent.TimeUnit.MILLISECONDS;
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.awaitility.Awaitility.await;
+import static org.awaitility.Awaitility.to;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
@@ -11,19 +12,25 @@ import static org.junit.Assert.assertThat;
 import com.github.charithe.kafka.EphemeralKafkaBroker;
 import com.github.charithe.kafka.KafkaJunitRule;
 import io.dropwizard.testing.junit.DropwizardAppRule;
+
+import java.lang.invoke.MethodHandles;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.rules.RuleChain;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.subethamail.wiser.WiserMessage;
 import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceApplication;
 import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceConfiguration;
 import welcomememberemailservice.it.kafka.KafkaOffsets;
 import welcomememberemailservice.it.smtp.SmtpServerRule;
+import welcomememberemailservice.port.incoming.adapter.kafka.WelcomeEmailConsumer;
 
 public abstract class IntegrationTestBase {
 
+    private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().getClass());
     private static final String INTEGRATION_YML = resourceFilePath("integration.yml");
     private static final String KAFKA_HOST = "localhost";
     private static final int KAFKA_PORT = 9092;
@@ -74,9 +81,11 @@ public abstract class IntegrationTestBase {
     protected void publishMessageAndWaitToBeConsumed(String topic, String message, String groupId) {
         KafkaOffsets kafkaOffsets = new KafkaOffsets(KAFKA_HOST, KAFKA_RULE.helper().kafkaPort());
         long previousOffset = Math.max(kafkaOffsets.readOffset(topic, groupId), 0);
+
+        LOG.info("Publishing message {} to topic {}", message, topic);
         KAFKA_RULE.helper().produceStrings(topic, message);
-        await().atMost(10, SECONDS)
-                .pollInterval(300, MILLISECONDS)
-                .until(() -> kafkaOffsets.readOffset(topic, groupId), equalTo(previousOffset + 1));
+
+        LOG.info("Waiting for message to be consumed from topic {}", topic);
+        await().atMost(10, SECONDS).until(() -> kafkaOffsets.readOffset(topic, groupId), equalTo(previousOffset + 1));
     }
 }
