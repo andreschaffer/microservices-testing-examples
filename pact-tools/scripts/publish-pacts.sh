e17895a9fdb007171ec6c6d392e361bc6453d8c8
Dan Eidmark <dan.eidmark@gmail.com>
2019-08-29 20:07:52 +0200

    Update travis with the new flow

---
+++ pact-tools/scripts/publish-pacts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+docker run --net=host -v `pwd`/welcome-member-email-service/target/pacts:/target/pacts pact-cli publish /target/pacts --broker-base-url=localhost --consumer-app-version=1.0-SNAPSHOT
+docker run --net=host -v `pwd`/special-membership-service/target/pacts:/target/pacts pact-cli publish /target/pacts --broker-base-url=localhost --consumer-app-version=1.0-SNAPSHOT
+docker run --net=host -v `pwd`/credit-score-service/target/pacts:/target/pacts pact-cli publish /target/pacts --broker-base-url=localhost --consumer-app-version=1.0-SNAPSHOT
