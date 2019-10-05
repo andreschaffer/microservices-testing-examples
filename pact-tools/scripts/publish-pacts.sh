7dd310a2f12fee2f8982b19595fd2023bd49e87e
Andre Schaffer <andre.schaffer@gmail.com>
2019-10-05 11:14:59 +0200

    Remove unused publish-pacts script

--- pact-tools/scripts/publish-pacts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker run --net=host -v `pwd`/welcome-member-email-service/target/pacts:/target/pacts pact-cli publish /target/pacts --broker-base-url=localhost --consumer-app-version=1.0-SNAPSHOT
-docker run --net=host -v `pwd`/special-membership-service/target/pacts:/target/pacts pact-cli publish /target/pacts --broker-base-url=localhost --consumer-app-version=1.0-SNAPSHOT
