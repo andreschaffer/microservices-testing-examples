825d144debe4c083f6d33a6c983a5a5ee7b70343
Andre Schaffer <andre.schaffer@gmail.com>
2020-03-04 23:10:17 +0100

    Update imports

--- welcome-member-email-service/src/test/java/welcomememberemailservice/it/IntegrationTestBase.java
+++ welcome-member-email-service/src/test/java/welcomememberemailservice/it/IntegrationTestBase.java
@@ -1,21 +1,8 @@
 package welcomememberemailservice.it;
 
-import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
-import static java.util.concurrent.TimeUnit.MILLISECONDS;
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
-import static org.awaitility.Awaitility.to;
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.assertThat;
-
 import com.github.charithe.kafka.EphemeralKafkaBroker;
 import com.github.charithe.kafka.KafkaJunitRule;
 import io.dropwizard.testing.junit.DropwizardAppRule;
-
-import java.lang.invoke.MethodHandles;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.rules.RuleChain;
@@ -26,7 +13,17 @@ import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceApplication;
 import welcomememberemailservice.bootstrap.WelcomeMemberEmailServiceConfiguration;
 import welcomememberemailservice.it.kafka.KafkaOffsets;
 import welcomememberemailservice.it.smtp.SmtpServerRule;
-import welcomememberemailservice.port.incoming.adapter.kafka.WelcomeEmailConsumer;
+
+import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.List;
+
+import static io.dropwizard.testing.ResourceHelpers.resourceFilePath;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 
 public abstract class IntegrationTestBase {
 
