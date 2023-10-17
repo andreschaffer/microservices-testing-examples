a58f9c704e878d2cf80cde50d1ca16f1a5395cde
André Schaffer <andre.schaffer@gmail.com>
2023-10-17 02:00:06 +0200

    Update pact provider tests

--- credit-score-service/src/test/java/creditscoreservice/it/pacts/verifications/HttpPactVerifications.java
+++ credit-score-service/src/test/java/creditscoreservice/it/pacts/verifications/HttpPactVerifications.java
@@ -5,13 +5,17 @@ import static creditscoreservice.it.pacts.PactConstants.PACT_BROKER_PORT;
 import static creditscoreservice.it.pacts.PactConstants.PACT_BROKER_URL;
 
 import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
-import au.com.dius.pact.provider.junitsupport.target.TestTarget;
 import creditscoreservice.it.IntegrationTestBase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 @Provider(CREDIT_SCORE_SERVICE)
 @IgnoreNoPactsToVerify
@@ -24,8 +28,16 @@ import creditscoreservice.it.IntegrationTestBase;
 )
 public class HttpPactVerifications extends IntegrationTestBase {
 
-  @TestTarget
-  public final HttpTestTarget target = new HttpTestTarget("localhost", SERVICE_RULE.getLocalPort());
+  @TestTemplate
+  @ExtendWith(PactVerificationInvocationContextProvider.class)
+  public void pactVerificationTestTemplate(PactVerificationContext context) {
+    context.verifyInteraction();
+  }
+
+  @BeforeEach
+  public void before(PactVerificationContext context) {
+    context.setTarget(new HttpTestTarget("localhost", SERVICE_RULE.getLocalPort()));
+  }
 
   @State("There is a tony.stark@example.com")
   public void tonyStarkCreditScore() {
--- special-membership-service/src/test/java/specialmembershipservice/it/pacts/verifications/MessagePactVerifications.java
+++ special-membership-service/src/test/java/specialmembershipservice/it/pacts/verifications/MessagePactVerifications.java
@@ -1,6 +1,8 @@
 package specialmembershipservice.it.pacts.verifications;
 
-import static com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder.okForJson;
+import static com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder.responseDefinition;
+import static com.github.tomakehurst.wiremock.common.ContentTypes.APPLICATION_JSON;
+import static com.github.tomakehurst.wiremock.common.ContentTypes.CONTENT_TYPE;
 import static java.util.Collections.singletonList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
@@ -10,14 +12,18 @@ import static specialmembershipservice.it.pacts.PactConstants.SPECIAL_MEMBERSHIP
 
 import au.com.dius.pact.provider.PactVerifyProvider;
 import au.com.dius.pact.provider.junit5.MessageTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
 import au.com.dius.pact.provider.junitsupport.IgnoreNoPactsToVerify;
 import au.com.dius.pact.provider.junitsupport.Provider;
 import au.com.dius.pact.provider.junitsupport.State;
 import au.com.dius.pact.provider.junitsupport.loader.PactBroker;
 import au.com.dius.pact.provider.junitsupport.loader.PactBrokerAuth;
-import au.com.dius.pact.provider.junitsupport.target.TestTarget;
 import jakarta.ws.rs.core.Response;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
 import specialmembershipservice.it.IntegrationTestBase;
 
 @Provider(SPECIAL_MEMBERSHIP_SERVICE)
@@ -31,14 +37,22 @@ import specialmembershipservice.it.IntegrationTestBase;
 )
 public class MessagePactVerifications extends IntegrationTestBase {
 
-  @TestTarget
-  public final MessageTestTarget target = new MessageTestTarget(
-      singletonList(this.getClass().getPackage().getName()));
+  @TestTemplate
+  @ExtendWith(PactVerificationInvocationContextProvider.class)
+  public void pactVerificationTestTemplate(PactVerificationContext context) {
+    context.verifyInteraction();
+  }
+
+  @BeforeEach
+  public void before(PactVerificationContext context) {
+    context.setTarget(new MessageTestTarget(singletonList(this.getClass().getPackage().getName())));
+  }
 
   @State("Tony Stark became a new member")
   public void tonyStarkBecameANewMember() {
     String email = "tony.stark@example.com";
-    setCreditResponse(email, okForJson(creditScoreDto(850)));
+    setCreditResponse(email, responseDefinition().withBody(creditScoreDto(850))
+        .withHeader(CONTENT_TYPE, APPLICATION_JSON));
     Map<String, Object> specialMembershipDto = specialMembershipDto(email);
     Response response = resourcesClient.postSpecialMembership(specialMembershipDto);
     response.close();
